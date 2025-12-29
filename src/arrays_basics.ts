9909a688101b36df80a2ab3fd8b94556da9dc49e
Abraham Bishop <crypticcodetechnologies@gmail.com>
2025-12-29 01:24:42 +0100

    Add arrays_basics.ts with examples of number arrays and mixed type arrays

---
+++ src/arrays_basics.ts
@@ -0,0 +1,7 @@
+
+const numbers: number[] = [1, 2, 3, 4, 5];
+// can also be written as:
+const moreNumbers: Array<number> = [6, 7, 8, 9, 10];
+
+// Mixed type array
+const mixedArray: (number | string)[] = [1, "two", 3, "four"];
